34a28bcb2a440e234c89594a8f538ae2f936002b
shw0471 <shw0471@naver.com>
2021-12-18 23:12:40 +0900

    update TokenResponse data class

---
+++ data/src/main/java/com/heewon/data/remote/response/TokenResponse.kt
@@ -0,0 +1,8 @@
+package com.heewon.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class TokenResponse(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("refresh_token") val refreshToken: String,
+)
