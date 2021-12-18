44bcfb451223b1eacf3484acb04e83ac74e0e096
shw0471 <shw0471@naver.com>
2021-12-18 23:13:27 +0900

    update AuthApi interface

---
+++ data/src/main/java/com/heewon/data/remote/api/AuthApi.kt
@@ -0,0 +1,25 @@
+package com.heewon.data.remote.api
+
+import com.heewon.data.remote.request.TokenRefreshRequest
+import com.heewon.data.remote.response.AuthLinkResponse
+import com.heewon.data.remote.response.TokenResponse
+import retrofit2.http.*
+
+interface AuthApi {
+
+    @GET("/oauth")
+    suspend fun fetchAuthLink(
+        @Query("type") authType: String
+    ): AuthLinkResponse
+
+    @POST("/auth")
+    suspend fun signIn(
+        @Query("code") code: String,
+        @Query("type") authType: String,
+    ): TokenResponse
+
+    @PUT("/auth")
+    suspend fun tokenRefresh(
+        @Body tokenRefreshRequest: TokenRefreshRequest
+    ): TokenResponse
+}
