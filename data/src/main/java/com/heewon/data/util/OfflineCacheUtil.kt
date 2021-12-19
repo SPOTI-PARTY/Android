c122d39fe38ed7d15f90c7eef51e12ca22ea4ba1
shw0471 <shw0471@naver.com>
2021-12-19 23:06:20 +0900

    update OfflineCacheUtil class

---
+++ data/src/main/java/com/heewon/data/util/OfflineCacheUtil.kt
@@ -0,0 +1,52 @@
+package com.heewon.data.util
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.lang.NullPointerException
+
+class OfflineCacheUtil<T> {
+
+    private lateinit var fetchLocalData: suspend () -> T
+    private lateinit var fetchRemoteData: suspend () -> T
+    private var isNeedRefresh: suspend (localData: T, remoteData: T) -> Boolean =
+        { localData, remoteData -> localData != remoteData }
+    private lateinit var refreshLocalData: suspend (remoteData: T) -> Unit
+
+    fun localData(fetchLocalData: suspend () -> T): OfflineCacheUtil<T> {
+        this.fetchLocalData = fetchLocalData
+        return this
+    }
+
+    fun remoteData(fetchRemoteData: suspend () -> T): OfflineCacheUtil<T> {
+        this.fetchRemoteData = fetchRemoteData
+        return this
+    }
+
+    fun compareData(isNeedRefresh: suspend (localData: T, remoteData: T) -> Boolean): OfflineCacheUtil<T> {
+        this.isNeedRefresh = isNeedRefresh
+        return this
+    }
+
+    fun doOnNeedRefresh(refreshLocalData: suspend (remoteData: T) -> Unit): OfflineCacheUtil<T> {
+        this.refreshLocalData = refreshLocalData
+        return this
+    }
+
+    fun createFlow(): Flow<T> =
+        flow {
+            try {
+                val localData = fetchLocalData()
+                emit(localData)
+
+                val remoteData = fetchRemoteData()
+                if (isNeedRefresh(localData, remoteData)) {
+                    refreshLocalData(remoteData)
+                    emit(remoteData)
+                }
+            } catch (e: NullPointerException) {
+                val remoteData = fetchRemoteData()
+                refreshLocalData(remoteData)
+                emit(remoteData)
+            }
+        }
+}
